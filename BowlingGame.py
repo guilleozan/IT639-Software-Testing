b8e41c8a64bf793836639ab2ad6cc087ac1fa8ed
Guillermo Ozan <guilleozan25@gmail.com>
2023-12-02 17:08:26 +1300

    All functions renamed
    All functions has been renamed with proper python formalities

--- BowlingGame.py
+++ BowlingGame.py
@@ -28,15 +28,19 @@ class BowlingGame:
         return self.rolls[rollIndex] == 10
     
     def is_spare(self, rollIndex):
+        # Check if the consecutive rolls form a spare.
         return self.rolls[rollIndex]+ self.rolls[rollIndex+1]==10
     
     def sticke_score(self,rollIndex):
+        # The function calculate the score for a strike frame.
         return  10+ self.rolls[rollIndex+1]+ self.rolls[rollIndex+2]
     
 
     def spare_score(self,rollIndex):
+        # Calculate the score for a spare frame.
         return  10+ self.rolls[rollIndex+2]
 
     def frame_score(self, rollIndex):
+        # calculate the score for a normal frame.
         return self.rolls[rollIndex] + self.rolls[rollIndex + 1]
 		
