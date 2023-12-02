3c28871f87aa90622e261407d828a71223b8c0dd
Guillermo Ozan <guilleozan25@gmail.com>
2023-12-02 17:47:27 +1300

    Corections to the Bowling functions and naming
    Changed the typo in method name from sticke_score to strike_score
    Fixed the indentation of the return result statement inside the score method
    Corrected the parameter names in the methods is_strike and is_spare

--- BowlingGame.py
+++ BowlingGame.py
@@ -1,46 +1,48 @@
-
 class BowlingGame:
     def __init__(self):
         # Initialize a new bowling game
-        self.rolls=[]
+        self.rolls = []
 
-    def roll(self,pins):
+    def roll(self, pins):
+        # Record the roll of a bowling ball.
         self.rolls.append(pins)
 
     def score(self):
         # Calculate and return the total score of the game
         result = 0
-        rollIndex=0
-        for frameIndex in range(10):
-            if frameIndex in range(10):
-                result += self.StrikeScore(rollIndex)
-                rollIndex +=1
-            elif self.isSpare(rollIndex):
-                result += self.spareScore(rollIndex)
-                rollIndex +=2
-            else:
-                result += self.frameScore(rollIndex)
-            rollIndex +=2
-            return result
+        roll_index = 0
 
-    def is_strike(self, rollIndex):
+        for frame_index in range(10):
+            if roll_index < len(self.rolls):
+
+                if self.is_strike(roll_index):
+                    result += self.strike_score(roll_index)
+                    roll_index += 1
+                elif self.is_spare(roll_index):
+                    result += self.spare_score(roll_index)
+                    roll_index += 2
+                else:
+                    result += self.frame_score(roll_index)
+                    roll_index += 2
+
+        return result
+
+    def is_strike(self, roll_index):
         # Check if the roll at the given index is a strike
-        return self.rolls[rollIndex] == 10
-    
-    def is_spare(self, rollIndex):
+        return self.rolls[roll_index] == 10
+
+    def is_spare(self, roll_index):
         # Check if the consecutive rolls form a spare.
-        return self.rolls[rollIndex]+ self.rolls[rollIndex+1]==10
-    
-    def sticke_score(self,rollIndex):
+        return self.rolls[roll_index] + self.rolls[roll_index + 1] == 10
+
+    def strike_score(self, roll_index):
         # The function calculate the score for a strike frame.
-        return  10+ self.rolls[rollIndex+1]+ self.rolls[rollIndex+2]
-    
+        return 10 + self.rolls[roll_index + 1] + self.rolls[roll_index + 2]
 
-    def spare_score(self,rollIndex):
+    def spare_score(self, roll_index):
         # Calculate the score for a spare frame.
-        return  10+ self.rolls[rollIndex+2]
+        return 10 + self.rolls[roll_index + 2]
 
-    def frame_score(self, rollIndex):
+    def frame_score(self, roll_index):
         # calculate the score for a normal frame.
-        return self.rolls[rollIndex] + self.rolls[rollIndex + 1]
-		
+        return self.rolls[roll_index] + self.rolls[roll_index + 1]
--- Test.py
+++ Test.py
@@ -10,31 +10,32 @@ class TestBowlingGame(unittest.TestCase):
         
         # This function set up the environment for each test
         self.game = BowlingGame.BowlingGame()
-
+   
     def test_gutter_game(self):
         # Test a scenario where no pins are knocked down in any roll of the game
         for i in range(0, 20):
-            self.game.rolls(0)
+            self.game.roll(0)
         assert self.game.score () == 0
         
+    breakpoint()
     def test_all_ones(self):
-        # test a game where all rolls knock down one pin.
+        # test a game where all roll knock down one pin.
         self.roll_many(1, 20)
         assert self.game.score () == 20
         
     def test_one_spare(self):
         # Test a game with a spare in the first frame
-        self.game.rolls(5)
-        self.game.rolls(5)
-        self.game.rolls(3)
+        self.game.roll(5)
+        self.game.roll(5)
+        self.game.roll(3)
         self.roll_many (0,17)
         assert self.game.score () == 16
         
     def test_one_strike(self):
         # Test a game with a strike in the first frame.
-        self.game.rolls(10)
-        self.game.rolls(4)
-        self.game.rolls(3)
+        self.game.roll(10)
+        self.game.roll(4)
+        self.game.roll(3)
         self.roll_many(0,16)
         assert  self.game.score () == 24
         
@@ -51,7 +52,7 @@ class TestBowlingGame(unittest.TestCase):
     def roll_many(self, pins,rolls):
         # Roll the ball multiple times with the same number of pins knocked down.
         for i in range(rolls):
-            self.game.rolls(pins)
+            self.game.roll(pins)
 			
 
 if __name__ == '__main__':
